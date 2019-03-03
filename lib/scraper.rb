063dcf75db7de1be82e9d82be30f2673b8a94a33
Alexander Baker <alex.baker685@gmail.com>
2019-03-03 20:42:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +15,10 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+     html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+     Nokogiri::HTML(html)
+  end
+
 end
-
-
-
